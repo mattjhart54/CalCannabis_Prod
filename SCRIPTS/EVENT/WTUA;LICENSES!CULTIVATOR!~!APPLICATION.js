9bb0abfbb78c1fe5b778fe39075a663ad9f98205
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2018-03-02 10:29:39 -0800

    Updates for Release 2.0

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -132,7 +132,7 @@ try{
 						if(!updOAltId.getSuccess()){
 							logDebug("Error updating Owner Alt Id: " + newOAltId + ":: " +updOAltId.getErrorMessage());
 						}else{
-							logDebug("newOAltId: " + updOAltId);
+							logDebug("newOAltId: " + newOAltId);
 							editAppSpecific("AltId", newOAltId,newODefId);
 							logDebug("Deficiency owner record ID updated to : " + newOAltId);
 						}
@@ -276,18 +276,22 @@ try{
 }
 
 //lwacht: 180207: story 2896: add a generic condition when a denial is appealed and remove when denial is done
+//lwacht: 180227: story 5291: add reference contact information to the condition as well
 try{
 	if(wfStatus=="Appealed" && wfTask =="Application Disposition"){
+		var condComment = capIDString;
 		var drpContact = getContactObj(capId,"Designated Responsible Party");
 		if(drpContact){
 			var drpSeqNbr = drpContact.refSeqNumber;
-			addContactStdCondition_rev(drpSeqNbr,"Application Condition", "Appeal Pending",capIDString);
+			condComment += "; Contact " + drpContact.people.firstName + " " + drpContact.people.lastName + " (ref ID: " + drpSeqNbr + ")";
+			addContactStdCondition_rev(drpSeqNbr,"Application Condition", "Appeal Pending",condComment);
 		}
 		var busContact = getContactObj(capId,"Business");
 		if(busContact){
 			var busSeqNbr = busContact.refSeqNumber;
 			if(busSeqNbr!=drpSeqNbr){
-				addContactStdCondition_rev(busSeqNbr,"Application Condition", "Appeal Pending",capIDString);
+				condComment += "; Contact " + busContact.people.firstName + " " + busContact.people.lastName + " (ref ID: " + busSeqNbr + ")";
+				addContactStdCondition_rev(busSeqNbr,"Application Condition", "Appeal Pending",condComment);
 			}else{
 				logDebug("Business and DRP are the same, not adding condition again.")
 			}
@@ -299,7 +303,8 @@ try{
 			if(ownContact){
 				var ownSeqNbr = ownContact.refSeqNumber;
 				if(ownSeqNbr!=busSeqNbr && ownSeqNbr!=drpSeqNbr){
-					addContactStdCondition_rev(ownSeqNbr,"Application Condition", "Appeal Pending",capIDString);
+					condComment += "; Contact " + ownContact.people.firstName + " " + ownContact.people.lastName + " (ref ID: " + ownSeqNbr + ")";
+					addContactStdCondition_rev(ownSeqNbr,"Application Condition", "Appeal Pending",condComment);
 				}else{
 					logDebug("Owner and (Business and/or DRP) are the same, not adding condition again.")
 				}
@@ -310,13 +315,13 @@ try{
 	aa.print("An error has occurred in WTUA:LICENSES/CULTIVATOR/*/APPLICATION: Add appeal denial condition: " + err.message);
 	aa.print(err.stack);
 }
-
+//lwacht: 180227: story 5291: end
 try{
 	if( wfTask =="Appeal"){
 		var drpContact = getContactObj(capId,"Designated Responsible Party");
 		var drpSeqNbr = drpContact.refSeqNumber;
 		var busContact = getContactObj(capId,"Business");
-		var busSeqNbr = drpContact.refSeqNumber;
+		var busSeqNbr = busContact.refSeqNumber;
 		var arrCond = getContactConditions_rev("Application Condition", "Applied", "Appeal Pending", null);
 		if(arrCond.length>0){
 			for (con in arrCond){
