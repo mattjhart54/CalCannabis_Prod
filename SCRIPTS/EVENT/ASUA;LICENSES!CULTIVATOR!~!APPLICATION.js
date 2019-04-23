13cd477879b5c4a639f42193728af1b9220206e7
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-04-23 14:56:10 -0700

    MJH story 5977 add code to Revoke a license

--- SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -58,14 +58,4 @@ try{
 	logDebug(err.stack);
 }
 
-//Mhart 04/23/2019 story 5977 revoke license
-try {
-	if(capStatus == "Revoked") {
-		var childId = getChildren("Licenses/Cultivator/*/Application");
-		for (x in childId) {}
-			updateAppStatus("Revoked",childId[x])
-	}
-}catch(err){
-	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Status Revoked: " + err.message);
-	logDebug(err.stack);
-}
+
--- SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!LICENSE.js
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!LICENSE.js
@@ -9,3 +9,14 @@ try{
 	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/LICENSE: Adding to CAT Set: " + err.message);
 	logDebug(err.stack);
 }
+//Mhart 04/23/2019 story 5977 revoke license
+try {
+	if(capStatus == "Revoked") {
+		var childId = getChildren("Licenses/Cultivator/*/Application");
+		for (x in childId) {}
+			updateAppStatus("Revoked",childId[x])
+	}
+}catch(err){
+	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Status Revoked: " + err.message);
+	logDebug(err.stack);
+}
