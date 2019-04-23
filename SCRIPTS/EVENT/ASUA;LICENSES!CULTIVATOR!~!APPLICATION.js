334ca839d9be39c6546ba9d4431e6a5d835419fa
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-04-23 14:54:26 -0700

    MJH  story 5977 added code for Revoked status

--- SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -32,7 +32,7 @@ try{
 
 	}
 }catch(err){
-	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Generic notifications: " + err.message);
+	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: status withdrawn: " + err.message);
 	logDebug(err.stack);
 } 
 
@@ -54,7 +54,18 @@ try{
 		}
 	}
 }catch(err){
-	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Generic notifications: " + err.message);
+	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: status voided: " + err.message);
 	logDebug(err.stack);
 }
 
+//Mhart 04/23/2019 story 5977 revoke license
+try {
+	if(capStatus == "Revoked") {
+		var childId = getChildren("Licenses/Cultivator/*/Application");
+		for (x in childId) {}
+			updateAppStatus("Revoked",childId[x])
+	}
+}catch(err){
+	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Status Revoked: " + err.message);
+	logDebug(err.stack);
+}
