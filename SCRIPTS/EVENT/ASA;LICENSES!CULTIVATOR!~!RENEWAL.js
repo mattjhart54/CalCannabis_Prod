6215dd2511bed10f43c8b5c755a276671b5c5c05
isallam <isallam@gmail.com>
2019-10-11 13:58:35 -0400

    copied from supp/test repository

---
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -0,0 +1,146 @@
+//lwacht
+//update AltId
+//lwacht: commenting out and putting in CTRCA
+
+try{
+	var partialCapId = getIncompleteCapId();
+	var parentCapString = "" + aa.env.getValue("ParentCapID");
+	parentArray = parentCapString.split("-");
+	parentCapId = aa.cap.getCapID(parentArray[0], parentArray[1], parentArray[2]).getOutput();
+	var parentAltId = parentCapId.getCustomID();
+	pCap = aa.cap.getCap(parentCapId).getOutput();
+	var pStatus = pCap.getCapStatus();
+	//1. Check to see if license is ready for renew
+	if (isRenewProcess(parentCapId, partialCapId)){
+		logDebug("CAPID(" + parentCapId + ") is ready for renew. PartialCap (" + partialCapId + ")");
+	//2. Associate partial cap with parent CAP.
+		var result = aa.cap.createRenewalCap(parentCapId, partialCapId, true);
+		if (result.getSuccess()){
+	//3. Copy key information from parent license to partial cap
+			pInfo = new Array;
+			loadAppSpecific(pInfo,parentCapId); 
+			editAppSpecific("License Number",parentAltId);
+			editAppSpecific("License Type", pInfo["License Type"]);
+			editAppSpecific("Business Name", pInfo["Legal Business Name"]);
+			editAppSpecific("Premises Address", pInfo["Premise Address"]);
+			editAppSpecific("APN", pInfo["APN"]);
+			editAppSpecific("Cultivator Type", pInfo["Cultivator Type"]);
+	//		editAppSpecific("Parent ID",parentCapId);
+			copyContactsByType(parentCapId,partialCapId,"Designated Responsible Party");
+			b1ExpResult = aa.expiration.getLicensesByCapID(parentCapId);
+			if (b1ExpResult.getSuccess()) {
+				this.b1Exp = b1ExpResult.getOutput();
+				expDate = this.b1Exp.getExpDate();	
+				if(expDate) {
+					tmpExpDate = expDate.getMonth() + "/" + expDate.getDayOfMonth() + "/" + expDate.getYear();
+					editAppSpecific("Expiration Date", tmpExpDate);
+				}
+			}
+		//4. Assess Renewal Fee
+			voidRemoveAllFees();
+			var feeDesc = pInfo["License Type"] + " - Renewal Fee";
+			var thisFee = getFeeDefByDesc("LIC_CC_REN", feeDesc);
+			if(thisFee){
+				updateFee(thisFee.feeCode,"LIC_CC_REN", "FINAL", 1, "Y", "N");
+			}else{
+				aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASA:Licenses/Cultivation/Licnese/Renewal: Add Fees: " + startDate, "fee description: " + feeDesc + br + "capId: " + capId + br + currEnv);
+				logDebug("An error occurred retrieving fee item: " + feeDesc);
+			}
+			var curDate = new Date();
+			var tmpDate = new Date(tmpExpDate);
+			if(tmpDate <= curDate) {
+				var feeDesc = pInfo["License Type"] + " - Late Fee";
+				var thisFee = getFeeDefByDesc("LIC_CC_REN", feeDesc);
+				if(thisFee){
+					updateFee(thisFee.feeCode,"LIC_CC_REN", "FINAL", 1, "Y", "N");
+					}else{
+					aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASA:Licenses/Cultivation/Licnese/Renewal: Add Fees: " + startDate, "fee description: " + feeDesc + br + "capId: " + capId + br + currEnv);
+					logDebug("An error occurred retrieving fee item: " + feeDesc);
+				}
+			}
+				
+		//5. Set B1PERMIT.B1_ACCESS_BY_ACA to "N" for partial CAP to not allow that it is searched by ACA user.
+	//		aa.cap.updateAccessByACA(partialCapId, "N");
+		}else{
+			aa.print("ERROR: Associate partial cap with parent CAP. " + result.getErrorMessage());
+		}
+	}
+} catch(err){
+	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/* /RENEWAL: Update AltId: " + err.message);
+	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in ASA:LICENSES/CULTIVATOR/* /RENEWAL: Submission: "+ startDate, capId + br + err.message+ br+ err.stack + br + currEnv);
+}
+
+function isRenewProcess(parentCapID, partialCapID)
+{
+	//1. Check to see parent CAP ID is null.
+	if (parentCapID == null || partialCapID == null 
+		|| aa.util.instanceOfString(parentCapID))
+	{
+		return false;
+	}
+	//2. Get CAPModel by PK for partialCAP.
+	var result = aa.cap.getCap(partialCapID);
+	if(result.getSuccess())
+	{
+		capScriptModel = result.getOutput();
+		//2.1. Check to see if it is partial CAP.	
+		if (capScriptModel.isCompleteCap())
+		{
+			aa.print("ERROR: It is not partial CAP(" + capScriptModel.getCapID() + ")");
+			return false;
+		}
+	}
+	else
+	{
+		aa.print("ERROR: Fail to get CAPModel (" + partialCapID + "): " + result.getErrorMessage());
+		return false;
+	}
+	//3.  Check to see if the renewal was initiated before. 
+	result = aa.cap.getProjectByMasterID(parentCapID, "Renewal", "Incomplete");
+	if(result.getSuccess())
+	{
+		partialProjects = result.getOutput();
+		if (partialProjects != null && partialProjects.length > 0)
+		{
+			//Avoid to initiate renewal process multiple times.
+			aa.print("Warning: Renewal process was initiated before. ( "+ parentCapID + ")");
+			return false;
+		}
+		
+	}
+	//4 . Check to see if parent CAP is ready for renew.
+	return isReadyRenew(parentCapID);
+}
+
+function isReadyRenew(capid)
+{
+	if (capid == null || aa.util.instanceOfString(capid))
+	{
+		return false;
+	}
+	var result = aa.expiration.isExpiredLicenses(capid);
+    if(result.getSuccess())
+	{
+		return true;
+	}  
+    else 
+    {
+      aa.print("ERROR: Failed to get expiration with CAP(" + capid + "): " + result.getErrorMessage());
+    }
+	return false;
+}
+
+function getIncompleteCapId() {
+	var s_id1 = aa.env.getValue("PermitId1");
+	var s_id2 = aa.env.getValue("PermitId2");
+	var s_id3 = aa.env.getValue("PermitId3");
+	var result = aa.cap.getCapIDModel(s_id1, s_id2, s_id3);
+	if(result.getSuccess()){
+		return result.getOutput();
+	}else{
+		aa.print("ERROR: Failed to get capId: " + result.getErrorMessage());
+		return null;
+	}
+}
+
