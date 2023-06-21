0f6faa4050d8beb95d1deabf6b64207fa47c6a69
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-06-21 13:36:16 -0700

    Update ASA;LICENSES!CULTIVATOR!LICENSE CASE!~.js
    MJH: user Story 7534

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!LICENSE CASE!~.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!LICENSE CASE!~.js
@@ -1,6 +1,22 @@
 try {
-	if(appTypeArray[3] == 'NA') {
-		updateLicCase(AInfo["License Number"], capId)
+		if(appTypeArray[3] == 'NA') {
+		updateLicCase(AInfo["License Number"], capId);
+		if (AInfo['Case Renewal Type'] =="Renewal Hold"){
+			var licNum = getApplication(AInfo["License Number"]);
+			var renewalCapProject = getRenewalCapByParentCapIDForIncomplete(licNum);
+			if (renewalCapProject != null) {
+				var renCapId = renewalCapProject.getCapID();
+				if (renCapId.toString().contains("EST")){
+					var renewalCap = aa.cap.getCap(renCapId).getOutput();
+					var renewalCapId = renewalCap.getCapID();
+					var altId = renewalCapId.getCustomID();
+					var removeFeesResult = voidRemoveAllFees(renewalCapId);
+					if (removeFeesResult){
+						logDebug("Removed fees from Record, " + altId);
+					}
+				}
+			}
+		}
 	}
 	else {
 		for(i in LICENSENUMBERS) {
@@ -18,25 +34,8 @@ try {
 			updateLicCase(LICENSENUMBERS[i]["License Number"], lcId);
 		}
 	}
-	if (AInfo['Case Renewal Type'] =="Renewal Hold"){
-		var licNum = getApplication(AInfo["License Number"]);
-		var renewalCapProject = getRenewalCapByParentCapIDForIncomplete(licNum);
-		if (renewalCapProject != null) {
-			var renCapId = renewalCapProject.getCapID();
-			if (renCapId.toString().contains("EST")){
-				var renewalCap = aa.cap.getCap(renCapId).getOutput();
-				var renewalCapId = renewalCap.getCapID();
-				var altId = renewalCapId.getCustomID();
-				var removeFeesResult = voidRemoveAllFees(renewalCapId);
-				if (removeFeesResult){
-					logDebug("Removed fees from Record, " + altId);
-				}
-			}
-		}
-	}
 			
 }catch(err){
 	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/License Case/*: " + err.message);
 	logDebug(err.stack);
 }
-
