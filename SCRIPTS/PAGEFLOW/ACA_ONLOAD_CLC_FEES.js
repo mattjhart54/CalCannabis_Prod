adaff6dbf8c4f2e24f7975485a5801b86f97e6ee
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2024-03-06 10:34:52 -0800

    Update ACA_ONLOAD_CLC_FEES.js

--- SCRIPTS/PAGEFLOW/ACA_ONLOAD_CLC_FEES.js
+++ SCRIPTS/PAGEFLOW/ACA_ONLOAD_CLC_FEES.js
@@ -173,7 +173,7 @@ try {
 				qty = (parseInt(newSqft) - base) / 2000;
 				thisFee = getFeeDefByDesc("LIC_CC_EXP", feeDesc);
 				if(AInfo["Limited Operation"] != "Yes") {
-					overFeeAmt = ((thisFee.formula*parse(qty))/365)*feeQty;
+					overFeeAmt = ((thisFee.formula*parseInt(qty))/365)*feeQty;
 				}else {
 					overFeeAmt = (((thisFee.formula*parseInt(qty))/365)*feeQty)*.2;
 				}			
